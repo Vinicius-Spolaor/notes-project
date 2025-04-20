f5ba23890fb7a9e929b1ee0395e36f9eb236e2ec
Vinicius-Spolaor <vinisspy@gmail.com>
2025-04-20 18:00:41 -0300

    small back changes

--- backend/src/main/java/com/ensolvers/challenge/config/WebConfig.java
+++ backend/src/main/java/com/ensolvers/challenge/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://notes-project-frontend.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
